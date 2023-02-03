b3b9b4d4133a1489f4801c5e41d39dce09c7575c
YR <yrlim1212@naver.com>
2023-02-03 17:35:59 +0900

    230203 결제

--- src/main/java/kr/co/jhta/controller/ProductDetailController.java
+++ src/main/java/kr/co/jhta/controller/ProductDetailController.java
@@ -6,23 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.co.jhta.dto.PayDTO;
 import kr.co.jhta.dto.ProductDTO;
+import kr.co.jhta.service.PayService;
 import kr.co.jhta.service.ProductDetailService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-@RequestMapping("/product")
+
 public class ProductDetailController {
+	
+	@Autowired
+	PayService payservice;
 	
 	@Autowired
 	 ProductDetailService ps;
 	
 	
-		@GetMapping("/productDetail")
+		@GetMapping("product/productDetail")
 		public String product(@RequestParam("p_no")int p_no, Model model) {
 			
 			List<ProductDTO> list = ps.selectOne(p_no);
@@ -31,12 +38,23 @@ public class ProductDetailController {
 			model.addAttribute("list",list);
 			return "mn_productDetail";
 		}
-		
-		@GetMapping("/pay")// 주소창
+
+// 상품상세보기에서 참여하기 누르면 결제페이지로 이동
+		@GetMapping("product/pay")// 주소창
 		public String pay(@RequestParam("p_no")int p_no, Model model) {
 			List<ProductDTO> list = ps.selectOne(p_no);
 			
 			model.addAttribute("list",list);
 			return "pay/payDetail";//보여지는 페이지 이동
+		}
+	
+// 결제페이지에서 결제완료 누르면 마이페이지로 이동
+		@PostMapping("/mypage") 
+		public String payOk(@ModelAttribute("dto2") PayDTO dto2) {
+			
+			System.out.println(dto2.getNickname());
+			
+			payservice.payAddOne(dto2);
+			return "/mypage";
 		}
 }
--- src/main/java/kr/co/jhta/dto/PayDTO.java
+++ src/main/java/kr/co/jhta/dto/PayDTO.java
@@ -1,5 +1,17 @@
 package kr.co.jhta.dto;
 
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
 public class PayDTO {
 
+	private String nickname;
+	private int p_no;
+	private Date pay_date;
+	private String pay_method;
+	private int pay_no;
+	private int pay_status;
+	private int price;
 }
